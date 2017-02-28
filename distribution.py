e1a9e163013597c4c8368ccb3aa9eddbc7c07fcf
jasper-meyer <jaspermeyer@hanovernorwichschools.org>
2017-02-28 14:46:06 -0500

    Updated from Brython Server: 2/28/2017 2:46:04 PM

--- distribution.py
+++ distribution.py
@@ -49,7 +49,16 @@ def compare(a, b):
     if a[0]>b[0] and a[1]>b[1]:
         return True
     elif a[0]==b[0] and b[1]>a[1]:
+        return b>a
+    elif a[0]>b[0] and a[1]<b[1]:
+        return a>b
+    elif a[0]==b[0] and b[1]<a[1]:
         return b<a
+    elif a[0]<b[0] and a[1]>b[1]:
+        return b>a
+    elif a[0]<b[0] and a[1]<b[1]:
+        return b>a
+        
 
 
 
