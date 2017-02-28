5bc0342a3362f8831186776fdc7bf12bd8e874d0
jasper-meyer <jaspermeyer@hanovernorwichschools.org>
2017-02-28 14:35:29 -0500

    Updated from Brython Server: 2/28/2017 2:35:27 PM

--- distribution.py
+++ distribution.py
@@ -46,7 +46,10 @@ l=[ac,bc,cc]
 z=list(zip(l,['a','b','c']))
 
 def compare(a, b):
-    return a[0]>b[0]
+    if a[0]>b[0] and a[1]>b[1]:
+        return True
+    elif a[0]=b[0] and a[1]<b[1]:
+        return a[0]<b[0]
 
 
 
