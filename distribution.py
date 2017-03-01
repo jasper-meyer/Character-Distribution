59addaf92bfc067ba835fbb50b4a2dd92e59d1ef
jasper-meyer <jaspermeyer@hanovernorwichschools.org>
2017-03-01 13:27:43 -0500

    Updated from Brython Server: 3/1/2017 1:27:41 PM

--- distribution.py
+++ distribution.py
@@ -38,12 +38,36 @@ Notice about this example:
 """
 t=input ("Please enter a string of text (the bigger the better): ")
 s=list(t)
-ac=(s.count('a'))
-bc=(s.count('b'))
+ca=(s.count('a'))
+cb=(s.count('b'))
 cc=(s.count('c'))
+cd=(s.count('d'))
+ce=(s.count('e'))
+cf=(s.count('f'))
+cg=(s.count('g'))
+ch=(s.count('h'))
+ci=(s.count('i'))
+cj=(s.count('j'))
+ck=(s.count('k'))
+cl=(s.count('l'))
+cm=(s.count('m'))
+cn=(s.count('n'))
+co=(s.count('o'))
+cp=(s.count('p'))
+cq=(s.count('q'))
+cr=(s.count('r'))
+cs=(s.count('s'))
+ct=(s.count('t'))
+cu=(s.count('u'))
+cv=(s.count('v'))
+cw=(s.count('w'))
+cx=(s.count('x'))
+cy=(s.count('y'))
+cz=(s.count('z'))
 
-l=[ac,bc,cc]
-z=list(zip(l,['a','b','c']))
+
+l=[ca,cb,cc,cd,ce,cf,cg,ch,ci,cj,ck,cl,cm,cn,co,cp,cq,cr,cs,ct,cu,cv,cw,cx,cy,cz]
+z=list(zip(l,['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']))
 
 def compare(a, b):
     if a[0]>b[0]:
@@ -68,5 +92,34 @@ def bsort(seq, cmp):
 
 
 bsort(z, compare)
-print(z)
+
+p,q=zip(*z)
+
+print (int(p[0])*(q[0]))
+print (int(p[1])*(q[1]))
+print (int(p[2])*(q[2]))
+print (int(p[3])*(q[3]))
+print (int(p[4])*(q[4]))
+print (int(p[5])*(q[5]))
+print (int(p[6])*(q[6]))
+print (int(p[7])*(q[7]))
+print (int(p[8])*(q[8]))
+print (int(p[9])*(q[9]))
+print (int(p[10])*(q[10]))
+print (int(p[11])*(q[11]))
+print (int(p[12])*(q[12]))
+print (int(p[13])*(q[13]))
+print (int(p[14])*(q[14]))
+print (int(p[15])*(q[15]))
+print (int(p[16])*(q[16]))
+print (int(p[17])*(q[17]))
+print (int(p[18])*(q[18]))
+print (int(p[19])*(q[19]))
+print (int(p[20])*(q[20]))
+print (int(p[21])*(q[21]))
+print (int(p[22])*(q[22]))
+print (int(p[23])*(q[23]))
+print (int(p[24])*(q[24]))
+print (int(p[25])*(q[25]))
+
 
